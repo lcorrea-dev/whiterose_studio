3d7e9e27a354026e30e27f462795cccd5787d2c4
Luis Correa Bruna <lu.correab@alumnos.duoc.cl>
2020-10-20 17:45:11 -0300

    feat: basic home and gallery urls mapping

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+
+def gallery(request):
+    return HttpResponse('<h1>Blog Gallery</h1>')
