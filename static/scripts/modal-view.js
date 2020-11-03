2de8688c647d148f534d550f0817a3ef4648cd36
Luis Correa Bruna <lu.correab@alumnos.duoc.cl>
2020-11-03 15:15:58 -0300

    feat: modal with author

--- static/scripts/modal-view.js
+++ static/scripts/modal-view.js
@@ -2,14 +2,18 @@ var modal = document.getElementById('myModal');
 
 var imgs = document.getElementsByClassName('screenshot');
 
+
 var modalImg = document.getElementById('img01');
-var captionText = document.getElementById('caption');
+var caption = document.getElementById('caption');
+
+var authorText = document.getElementById('author');
 
 for (var img of imgs) {
     img.onclick = function () {
         modal.style.display = 'block';
         modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
+        caption.innerText= this.nextElementSibling.innerText;
+        author.innerText= 'by ' + this.nextElementSibling.nextElementSibling.innerText
     };
 }
 
